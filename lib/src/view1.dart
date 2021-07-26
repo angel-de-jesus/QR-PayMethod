343144e8a135708e1b43da9d1a5cb025562b73b1
Angel Gonzalez <173237@ids.upchiapas.edu.mx>
2021-07-25 21:48:09 -0500

    v3

--- lib/src/view1.dart
+++ lib/src/view1.dart
@@ -7,7 +7,8 @@ class MyPay extends StatefulWidget {
 }
 
 class _PayMethod extends State<MyPay> {
-  int saldo = 1000;
+  int saldo = 10200;
+  int valor = 0;
   String texto ="";
 
   Widget build(BuildContext context) {
@@ -18,21 +19,25 @@ class _PayMethod extends State<MyPay> {
           actions: [],
           title: Text("Pay Method QR"),
         ),
-
-        
+ 
         body: Column(
           children: [
             Text("\nSALDO:", textAlign: TextAlign.right, style: TextStyle(fontSize: 20),),
+
             Container(
               height: 200,
               width: 400,
+
               child:Card(
+                color: Colors.white,
                 semanticContainer: true,
                 clipBehavior: Clip.antiAliasWithSaveLayer,
-                child: Text("$saldo", style: TextStyle(fontSize: 25),textAlign: TextAlign.center, ),
+                child: Text("\n"'$saldo', style: TextStyle(fontSize: 45, color: Colors.black),textAlign: TextAlign.center, ),
+
                 shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(10.0),
                 ),
+
                 elevation: 5,
                 margin: EdgeInsets.all(10),
               )
@@ -43,8 +48,10 @@ class _PayMethod extends State<MyPay> {
               child: Column(mainAxisAlignment: MainAxisAlignment.center,children: [Icon(Icons.camera),Text("scanner")],), 
               onPressed: () async {
                 String barcodeScanRes = await FlutterBarcodeScanner.scanBarcode('#A03131','Cancelar',false,ScanMode.QR,);
-                setState(() {
-                  texto=barcodeScanRes;
+                texto=barcodeScanRes;
+                valor = int.parse(texto);               
+                setState(() { 
+                  saldo = saldo-valor;
                 });
               },
             ),
